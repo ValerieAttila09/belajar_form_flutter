1c479275d735330f54dc12f4a8e26ef7af6512ac
valerieattilaalfath <valerieattilaalfath@gmail.com>
2025-10-21 07:18:20 +0000

    belajar textfield dan tabel

---
+++ lib/counter.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: SetCounter());
+  }
+}
+
+class SetCounter extends StatefulWidget {
+  const SetCounter({super.key});
+
+  @override
+  State<SetCounter> createState() => _Counter();
+}
+
+class _Counter extends State<SetCounter> {
+  int number = 0;
+
+  void tambahAngka() {
+    setState(() {
+      number += 2;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login"),
+        backgroundColor: const Color.fromARGB(255, 33, 135, 175),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              number.toString(),
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 30,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                if (number < 10) {
+                  tambahAngka();
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 33, 135, 175),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+              ),
+              child: Text(
+                "+",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                  color: const Color.fromARGB(255, 255, 255, 255),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -9,23 +9,37 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: SetCounter());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TextfieldToTable(),
+    );
   }
 }
 
-class SetCounter extends StatefulWidget {
-  const SetCounter({super.key});
+class TextfieldToTable extends StatefulWidget {
+  const TextfieldToTable({super.key});
 
   @override
-  State<SetCounter> createState() => _Counter();
+  State<TextfieldToTable> createState() => TextfieldPageState();
 }
 
-class _Counter extends State<SetCounter> {
-  int number = 0;
+class TextfieldPageState extends State<TextfieldToTable> {
+  final TextEditingController namaController = TextEditingController();
+  final TextEditingController kelasController = TextEditingController();
 
-  void tambah_angka() {
+  // Array list
+
+  List<Map<String, String>> DataList = [];
+
+  void tambahData() {
     setState(() {
-      number += 2;
+      DataList.add({
+        'nama': namaController.text,
+        'kelas': kelasController.text,
+      });
+      // untuk membersihkan isi form atau textfield
+      namaController.clear();
+      kelasController.clear();
     });
   }
 
@@ -33,47 +47,77 @@ class _Counter extends State<SetCounter> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Login"),
-        backgroundColor: const Color.fromARGB(255, 33, 135, 175),
+        title: Text("Form Data"),
+        backgroundColor: Colors.blueGrey,
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              number.toString(),
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 30,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                if (number < 10) {
-                  tambah_angka();
-                }
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color.fromARGB(255, 33, 135, 175),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsetsGeometry.all(10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                TextFormField(
+                  controller: namaController,
+                  decoration: InputDecoration(
+                    labelText: "Nama Siswa",
+                    border: OutlineInputBorder(),
+                  ),
                 ),
-                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 40),
-              ),
-              child: Text(
-                "+",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
-                  color: const Color.fromARGB(255, 255, 255, 255),
+                const SizedBox(height: 10),
+                TextFormField(
+                  controller: kelasController,
+                  decoration: InputDecoration(
+                    labelText: "Kelas Siswa",
+                    border: OutlineInputBorder(),
+                  ),
                 ),
-              ),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: tambahData,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 33, 135, 175),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    padding: EdgeInsets.symmetric(vertical: 14, horizontal: 40),
+                  ),
+                  child: Text(
+                    "Tambah",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w400,
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: <Widget>[
+                      DataTable(
+                        columns: const <DataColumn>[
+                          DataColumn(label: Text("No")),
+                          DataColumn(label: Text("Nama Siswa")),
+                          DataColumn(label: Text("Kelas")),
+                        ],
+                        rows: List.generate(
+                          DataList.length,
+                          (index) => DataRow(cells: [
+                            DataCell(Text((index + 1).toString())),
+                            DataCell(Text(DataList[index]['nama']!)),
+                            DataCell(Text(DataList[index]['kelas']!))
+                          ]),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
